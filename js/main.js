ba4d9f12fa2868149661528a35936048a72757f7
samrozaliofficial <samrozaliofficial@gmail.com>
2025-12-25 15:26:28 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Samroz Portfolio Loaded");
+document.querySelectorAll('.reveal').forEach((el)=>{
+  gsap.from(el,{
+    opacity:0,
+    y:80,
+    duration:1.2,
+    ease:"power4.out",
+    scrollTrigger:{
+      trigger:el,
+      start:"top 85%"
+    }
+  })
+})
