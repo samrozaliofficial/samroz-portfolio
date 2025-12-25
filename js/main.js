94568d1e76718562724e15efc00ca3da71387551
samrozaliofficial <samrozaliofficial@gmail.com>
2025-12-25 15:26:55 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ document.querySelectorAll('.reveal').forEach((el)=>{
     }
   })
 })
+document.querySelectorAll('.card').forEach(card=>{
+  gsap.to(card,{
+    y:-15,
+    repeat:-1,
+    yoyo:true,
+    duration:3,
+    ease:"sine.inOut"
+  })
+})
